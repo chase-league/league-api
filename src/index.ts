22b63824a4ca3d8c8536aa86301978f102d3e189
chasino <sirpumphrey@gmail.com>
2020-03-18 20:28:05 -0700

    added items endpoint

--- src/index.ts
+++ src/index.ts
@@ -20,11 +20,32 @@ let champions = [
   }
 ]
 
+
+let items = [
+  {
+    name: 'Longsword',
+    price: 350,
+    ad: 15,
+    ap: 0
+  },
+  {
+    name: 'Magic Tome',
+    price: 435,
+    ad: 0,
+    ap: 35
+  },
+]
+
 app.get('/', (req, res) => res.send('Hello World!!!!'))
 
 app.get('/champions', (req, res) => {
   console.log('retreiving all champions')
   res.json(champions)
+})
+
+app.get('/items', (req, res) => {
+  console.log('retreiving all items')
+  res.json(items)
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
